f1da6465df6d3c5daafcd64bf70effc117902765
jinumona <jinumona7@gmail.com>
2022-09-18 08:59:11 +0000

    userdata updated

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ cat <<EOF > /var/www/html/index.php
 <?php
 \$output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>\$output</pre></center></h1>";
-echo "<h1><center><pre>  Version1  </pre></center></h1>";
+echo "<h1><center><pre>  Version2  </pre></center></h1>";
 ?>
 EOF
